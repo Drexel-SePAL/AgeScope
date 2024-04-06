2e0771951ca40786504f98fa47df3dd1975403c4
Yifan Yao <fanfannnmn@protonmail.ch>
2024-04-06 15:13:10 -0500

    complete prototype of dynamic analysis

--- src/main/java/DynamicAnalyzer/Main.java
+++ src/main/java/DynamicAnalyzer/Main.java
@@ -1,27 +1,24 @@
 package DynamicAnalyzer;
 
-import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.openqa.selenium.By;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 public class Main {
     private static AndroidDriver driver;
 
     public static void setupDriver() throws URISyntaxException, MalformedURLException {
-        var options = new UiAutomator2Options()
-                .setPlatformName("Android")
-                .setPlatformVersion("13")
-                .setAutomationName("uiautomator2")
-                .setDeviceName("Pixel_6")
-                .setAutoGrantPermissions(true)
-                .setApp("/Users/fanfannnmn/Downloads/adult_apk/BCA0806E08712FB24983990B0FEBB649F28502E4C53985AA0F02EC0DFBE45C13.apk");
-        driver = new AndroidDriver(
-                new URI("http://127.0.0.1:4723").toURL(), options
-        );
+        var options = new UiAutomator2Options().setPlatformName("Android").setPlatformVersion("13").setAutomationName("uiautomator2").setDeviceName("Pixel_6").setAutoGrantPermissions(true).setApp("/Users/fanfannnmn/Downloads/adult_apk/BCA0806E08712FB24983990B0FEBB649F28502E4C53985AA0F02EC0DFBE45C13.apk");
+        driver = new AndroidDriver(new URI("http://127.0.0.1:4723").toURL(), options);
     }
 
     public static void tearDownDriver() throws InterruptedException {
@@ -29,34 +26,68 @@ public class Main {
         driver.quit();
     }
 
-    public static void getTextByClassName(String className) {
-        var els = driver.findElements(AppiumBy.className(className));
-        for (var el : els) {
-            var text = el.getText().strip();
-            if (!text.isEmpty() && !text.equals("null")) {
-                System.out.println(el.getText());
+    public static ArrayList<String> getText(Document doc) {
+        var elementsWithText = doc.select("[text], [content-desc]");
+
+        var textList = new ArrayList<String>();
+        for (var element : elementsWithText) {
+            var text = element.attr("text").trim();
+            var contentDesc = element.attr("content-desc").trim();
+            if (!text.isEmpty()) {
+                textList.add(text);
+            }
+            if (!contentDesc.isEmpty()) {
+                textList.add(contentDesc);
             }
         }
+
+        return textList;
     }
 
-    public static void getAttrByClassName(String className) {
-        var els = driver.findElements(AppiumBy.className(className));
-        for (var el : els) {
-            var attr = el.getAttribute("content-desc").strip();
-            if (!attr.isEmpty() && !attr.equals("null")) {
-                System.out.println(attr);
+    public static boolean containAgeCheckText(ArrayList<String> list) {
+        var pattern = Pattern.compile("adult(s)?( only)?|(over )?[1-2]\\d\\+?|under( )?age|age of [1-2]\\d|af_num_adults");
+
+        for (var x : list) {
+            if (pattern.matcher(x).find()) {
+                return true;
             }
         }
+
+        return false;
     }
 
-    public static void main(String[] args) throws MalformedURLException, URISyntaxException, InterruptedException {
+    public static boolean hasAgeCheck(Document doc) {
+        var textList = getText(doc);
+
+        return containAgeCheckText(textList);
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
         setupDriver();
         Thread.sleep(10000);
 
-        getTextByClassName("android.widget.TextView");
-
-        getAttrByClassName("android.widget.CheckBox");
-        getAttrByClassName("android.view.View");
+        var mainDoc = Jsoup.parse(driver.getPageSource());
+        if (hasAgeCheck(mainDoc)) {
+            System.out.println("Contains in mainDoc");
+        } else {
+            var clickable = mainDoc.select("[clickable=true]");
+            System.out.println(clickable);
+            
+            var clickableElements = driver.findElements(By.xpath("//*[@clickable='true']"));
+            for (var element : clickableElements) {
+                try {
+                    element.click();
+                    var doc = Jsoup.parse(driver.getPageSource());
+                    if (hasAgeCheck(doc)) {
+                        System.out.println("Contains in otherDoc");
+                    }
+                } catch (Exception e) {
+                    System.out.println("Could not click on some elements due to overlay or state change.");
+                } finally {
+                    driver.navigate().back();
+                }
+            }
+        }
 
         tearDownDriver();
     }
