c91e2c729f5580b726f216ea0f9f66231882ba2d
Yifan Yao <fanfannnmn@protonmail.ch>
2025-02-08 00:57:33 -0600

    [skip ci] update execution script

--- docker_run.sh
+++ docker_run.sh
@@ -2,5 +2,5 @@
 
 set -ve
 
-find /sample -type f -name *.apk > /sample/apk_index.txt
+find /sample -type f -name "*.apk" > /sample/apk_index.txt
 java -cp app.jar StaticUIAnalyzer.Main -i /sample/apk_index.txt -o /sample/result -p /opt/android-sdk/platforms
